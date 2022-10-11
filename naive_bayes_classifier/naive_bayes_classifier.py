7407fdca604f9a0fad7235e1e067d44759164047
cacajr <realcarlos7@hotmail.com>
2022-10-11 12:21:25 -0300

    fixing naive bayes model and testing

--- naive_bayes_classifier/naive_bayes_classifier.py
+++ naive_bayes_classifier/naive_bayes_classifier.py
@@ -5,39 +5,62 @@ import numpy as np
 
 class naive_bayes_classifier:
     def __init__(self):
-        # will save the class probabilities of each class for each sample
-        self.__class_matrix_prob = pd.DataFrame([])
+        #                                                                                        attrb 1      attrb 2
+        # will save the mean and standard deviation of each attribute for each class: class 1 [(mean, std), (mean, std), ...]
+        #                                                                             class 2 [    ...    ,     ...    , ...]
+        self.__attribute_matrix_mean_std = pd.DataFrame([[]])
+        # will save the priori for each class
+        self.__priori = []  # [<priori_class_1>, <priori_class_2>, ...]
 
     def fit(self, X, y):
-        # initializing the class_matrix_prob
         classes = pd.Series(y.unique())
-        values = [[ 0 for _ in range(len(classes)) ] for _ in range(len(X))]
-        self.__class_matrix_prob = pd.DataFrame(values, columns=classes)
+        values = [[() for _ in range(len(X.columns))] for _ in range(len(classes))]
+        self.__attribute_matrix_mean_std = pd.DataFrame(values, columns=X.columns, index=classes)
 
-        for cls in self.__class_matrix_prob.columns:
+        for cls in self.__attribute_matrix_mean_std.index.values:
             i_cls_samples = np.where(y == cls)
             Xc = X.loc[i_cls_samples]
+            self.__priori.append(len(Xc) / len(X))  # insert priori for each group of samples separated per class
 
-            for i_sample, x in enumerate(X.values):
-                likelyhood = self.__likelyhood(x, Xc)
-                priori = len(Xc)/len(X)
-                self.__class_matrix_prob.loc[i_sample, cls] = likelyhood * priori
-
-        print(self.__class_matrix_prob)
+            for feature in X.columns:
+                column_mean = np.mean(Xc[feature])
+                column_std = np.std(Xc[feature])
+                self.__attribute_matrix_mean_std.loc[cls, feature] = (column_mean, column_std)
     
-    def __likelyhood(self, x, Xc):
-        lkh = 1
+    def predict(self, x):
+        if len(x) != len(self.__attribute_matrix_mean_std.columns):
+            raise Exception('Sample invalid')
 
-        for i_feature, feature in enumerate(Xc.columns):
-            column_mean = np.mean(Xc[feature])
-            column_std = np.std(Xc[feature])
-            lkh = lkh * self.__gaussian(x[i_feature], column_mean, column_std)
+        posteriors = [] # [[<posteriori>, <class>], ...]
 
-        return lkh
+        for i_cls, cls in enumerate(self.__attribute_matrix_mean_std.index.values):
+            likelihood = 1
+            for i_feature, feature in enumerate(self.__attribute_matrix_mean_std.columns):
+                column_mean = self.__attribute_matrix_mean_std.loc[cls, feature][0]
+                column_std = self.__attribute_matrix_mean_std.loc[cls, feature][1]
+                likelihood = likelihood * self.__gaussian(x[i_feature], column_mean, column_std)
+
+            posteriors.append([
+                likelihood * self.__priori[i_cls], 
+                cls
+            ])
+
+        biggest_posteriori_class = sorted(
+            posteriors,
+            key=lambda posteriors: posteriors[0] # sorted by posteriori
+        )[-1][1]
+
+        return biggest_posteriori_class
 
     def __gaussian(self, x, mu, sig):
-        prob = (1/np.sqrt(2*np.pi*sig)) * np.exp((-1/2) * ((x-mu)/sig)**2)
+        prob = (1 / (np.sqrt(2 * np.pi) * sig)) * np.exp((-1 / 2) * ((x - mu) / sig)**2)
         return prob
 
-    def predict(self, x):
-        pass
+    def score(self, X_test, y_test):
+        hits = 0
+
+        for sample, predict in zip(X_test.values, y_test.values):
+            if self.predict(sample) == predict:
+                hits += 1
+
+        return hits/y_test.size
