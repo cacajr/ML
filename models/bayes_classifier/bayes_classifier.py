2c74a45e56da50a9f66a3028efc4bfc757e0eb25
cacajr <realcarlos7@hotmail.com>
2022-10-30 22:46:27 -0300

    adding more features to bayes model

--- models/bayes_classifier/bayes_classifier.py
+++ models/bayes_classifier/bayes_classifier.py
@@ -3,7 +3,14 @@ import numpy as np
 
 
 class bayes_classifier:
-    def __init__(self):
+    ''' covariance_matrix_type params
+
+    default: use one covariance matrix for each set of classes
+    majority_class: uses the covariance matrix of the largest set of classes
+    mean_class: uses the average covariance matrix of the covariance matrices of each set of classes
+
+    '''
+    def __init__(self, covariance_matrix_type = 'default'):
         # The covariance matrix list will save one covariance matrix to each class set.
         self.__covariance_matrix_list = []  #[[<covariance_matrix_class_1>], [<covariance_matrix_class_2>], ...]
 
@@ -17,6 +24,8 @@ class bayes_classifier:
 
         # The noise matrix will save a matrix with diagonal with values equel to 1 * noise on the main diagonal and 0 on the rest
         self.__noise_matrix = [[]]
+
+        self.__covariance_matrix_type = covariance_matrix_type
 
         # will save the priori for each class
         self.__priori = []  # [<priori_class_1>, <priori_class_2>, ...]
@@ -32,9 +41,50 @@ class bayes_classifier:
             Xc = X.loc[i_cls_samples]
             self.__priori.append(len(Xc) / len(X))  # insert priori for each group of samples separated per class
 
-            self.__covariance_matrix_list.append(pd.DataFrame(np.cov(Xc, rowvar=False)))
             self.__mean_list.append(np.array([Xc[feat].mean() for feat in self.__feature_array]))
+
+        self.__covariance_calc(X, y)
     
+    def __covariance_calc(self, X, y):
+        if self.__covariance_matrix_type == 'default':
+            for cls in self.__class_array:
+                i_cls_samples = X.index[np.where(y == cls)] # this take the index returned for np and take the pd (X) index
+                Xc = X.loc[i_cls_samples]
+
+                self.__covariance_matrix_list.append(pd.DataFrame(np.cov(Xc, rowvar=False)))
+
+        elif self.__covariance_matrix_type == 'majority_class':
+            majority_class = [self.__class_array[0], 0] # [<majority_class>, <size>]
+
+            for cls in self.__class_array:
+                i_cls_samples = X.index[np.where(y == cls)] # this take the index returned for np and take the pd (X) index
+                if majority_class[1] < len(i_cls_samples):
+                    majority_class[0] = cls
+                    majority_class[1] = len(i_cls_samples)
+
+            i_cls_samples = X.index[np.where(y == majority_class[0])]
+            Xc = X.loc[i_cls_samples]
+
+            for cls in self.__class_array:
+                self.__covariance_matrix_list.append(pd.DataFrame(np.cov(Xc, rowvar=False)))
+
+        elif self.__covariance_matrix_type == 'mean_class':
+            covariance_matrix_list = []
+
+            for cls in self.__class_array:
+                i_cls_samples = X.index[np.where(y == cls)] # this take the index returned for np and take the pd (X) index
+                Xc = X.loc[i_cls_samples]
+
+                covariance_matrix_list.append(pd.DataFrame(np.cov(Xc, rowvar=False)))
+
+            covariance_matrix_mean = sum(covariance_matrix_list) / len(covariance_matrix_list)
+
+            for cls in self.__class_array:
+                self.__covariance_matrix_list.append(covariance_matrix_mean)
+
+        else:
+            raise Exception('Covariance matrix type invalid')
+
     def predict(self, x):
         if len(x) != len(self.__feature_array):
             raise Exception('Sample invalid')
